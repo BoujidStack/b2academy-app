e3fc20a2bc62d69707aae743287fc1caee7f40cc
hamza boujid <hamzaboujid24@gmail.com>
2023-11-21 17:52:20 +0100

    fix responsive

--- src/components/ourMethodology/OurMethodology.jsx
+++ src/components/ourMethodology/OurMethodology.jsx
@@ -26,37 +26,6 @@ const OurMethodology = () => {
                                 <p>{t('methodology.DecentralizedLwarning')}</p>
                             </div>
                         </div>
-                        <div className="content-point">
-                            <img src={logo} width={35} height={35} alt="Decentralized Learning" data-aos="zoom-in-up" data-aos-duration="1000" />
-                            <div data-aos="fade-left" data-aos-duration="1000">
-                                <h5>METAVERSE :</h5>
-                                <p>{t('methodology.Metaverse')}</p>
-                            </div>
-                        </div>
-                        <div className="content-point">
-                            <img src={logo} width={35} height={35} alt="Decentralized Learning" data-aos="zoom-in-up" data-aos-duration="1000" />
-                            <div data-aos="fade-left" data-aos-duration="1000">
-                                <h5>FLEXIBLE CURRICULUM :</h5>
-                                <p>{t('methodology.FlexibleCurriculum')}<br />
-                                    <strong> • {t('methodology.FlexibleCurriculum-point1')}</strong><br />
-                                    <strong> • {t('methodology.FlexibleCurriculum-point2')}</strong>
-                                </p>
-                            </div>
-                        </div>
-                        <div className="content-point">
-                            <img src={logo} width={35} height={35} alt="Decentralized Learning" data-aos="zoom-in-up" data-aos-duration="1000" />
-                            <div data-aos="fade-left" data-aos-duration="1000">
-                                <h5>MICROLEARNING : </h5>
-                                <p>{t('methodology.MicroLearning')}</p>
-                            </div>
-                        </div>
-                        <div className="content-point">
-                            <img src={logo} width={35} height={35} alt="Decentralized Learning" data-aos="zoom-in-up" data-aos-duration="1000" />
-                            <div data-aos="fade-left" data-aos-duration="1000">
-                                <h5>NFT CERTIFICATION : </h5>
-                                <p>{t('methodology.NftCertification')}</p>
-                            </div>
-                        </div>
                     </div>
                     <div className="image-section">
                         <div className="card">
