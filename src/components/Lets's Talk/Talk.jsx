a694e92400ac67b1934695becd07ca12dbe2fed3
hamza boujid <hamzaboujid24@gmail.com>
2023-11-27 15:34:39 +0100

    fix responsive

--- src/components/Lets's Talk/Talk.jsx
+++ src/components/Lets's Talk/Talk.jsx
@@ -1,41 +1,65 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react';
 import './TalkStyles.css';
 import Newsletter from '../newsletter/Newsletter';
 import Fqa from '../FrequentlyAskedQuestions/Fqa';
 import Appointment from '../Appointment scheduling/Appointment';
-import { Link } from 'react-scroll'
+import { Link } from 'react-scroll';
 import { useTranslation } from 'react-i18next';
-import FooterTwo from '../footerForTalk&Bdp/FooterTwo';
 import Navbar from '../navbar/Navbar';
 import ContactUs from '../Contact Us/ContactUs';
 import Footer from '../footer/Footer';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Talk = () => {
-    const [nav, setNav] = useState(false)
-    const { t } = useTranslation();
-    const handleClose = () => {
-        setNav(!nav)
-    }
+  const { t } = useTranslation();
 
-    return (
-        <>
-            <Navbar hideNavbarLinks={true} />
-            <div name='talk' className='talk'>
-                <div className='content-talk'>
-                    <div className='centered-content'>
-                        <h1 data-aos="fade-down" data-aos-duration="1000" className='title-talk'>{t('talk.title')}</h1>
-                        <p data-aos="zoom-in" data-aos-duration="1000" className='text-talk' dangerouslySetInnerHTML={{ __html: t('talk.description') }}></p>
-                        <div data-aos="fade-up" data-aos-duration="1000"><Link onClick={handleClose} activeClass="active" to="appointment" spy={true} smooth={true} duration={500}><button className='centered-button'> {t('talk.button')}</button></Link></div>
-                    </div>
-                </div>
+  useEffect(() => {
+    AOS.init({
+    });
+  }, []);
+
+  return (
+    <>
+      <Navbar hideNavbarLinks={true} />
+      <div name='talk' className='talk'>
+        <div className='content-talk'>
+          <div className='centered-content'>
+            <h1
+              data-aos='fade-down'
+              data-aos-duration='1000'
+              className='title-talk'
+            >
+              {t('talk.title')}
+            </h1>
+            <p
+              data-aos='zoom-in'
+              data-aos-duration='1000'
+              className='text-talk'
+              dangerouslySetInnerHTML={{ __html: t('talk.description') }}
+            ></p>
+            <div data-aos='fade-up' data-aos-duration='1000'>
+              <Link
+                onClick={() => AOS.refreshHard()} 
+                activeClass='active'
+                to='appointment'
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                <button className='centered-button'>{t('talk.button')}</button>
+              </Link>
             </div>
-            <Appointment />
-            <Newsletter />
-            <ContactUs />
-            <Fqa />
-            <Footer hideFooterLinks={true} />
-        </>
-    );
+          </div>
+        </div>
+      </div>
+      <Appointment />
+      <Newsletter />
+      <ContactUs />
+      <Fqa />
+      <Footer hideFooterLinks={true} />
+    </>
+  );
 };
 
 export default Talk;
--- src/components/blockchaindeveloperprogram/Bdp.jsx
+++ src/components/blockchaindeveloperprogram/Bdp.jsx
@@ -1,36 +1,43 @@
-import React, { useState } from 'react'
-import './BdpStyles.css'
-import Newsletter from '../newsletter/Newsletter'
-import Fqa from '../FrequentlyAskedQuestions/Fqa'
-import ModalOverlayEnroll from '../ModalOverlayEnrollNow/ModalOverlayEnroll'
-import NumberCountingSection from '../NumberCountingSection/NumberCountingSection'
-import Enrolltoday from '../Enrolltoday/Enrolltoday'
-import Advantages from '../Advantages/Advantages'
-import Business from '../B2AcademyForBusiness/Business'
-import ProgramModules from '../programModules/ProgramModules'
+import React, { useState, useEffect } from 'react';
+import './BdpStyles.css';
+import Newsletter from '../newsletter/Newsletter';
+import Fqa from '../FrequentlyAskedQuestions/Fqa';
+import ModalOverlayEnroll from '../ModalOverlayEnrollNow/ModalOverlayEnroll';
 import { useTranslation } from 'react-i18next';
-import FooterTwo from '../footerForTalk&Bdp/FooterTwo'
-import Navbar from '../navbar/Navbar'
-import ContactUs from '../Contact Us/ContactUs'
-import Footer from '../footer/Footer'
+import Navbar from '../navbar/Navbar';
+import ContactUs from '../Contact Us/ContactUs';
+import Footer from '../footer/Footer';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Bdp = () => {
     const { t } = useTranslation();
     const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        AOS.init({
+        });
+    }, []);
+
     const handleCloseModal = () => {
         setShowModal(false);
     };
+
     return (
         <>
             <Navbar hideNavbarLinks={true} />
             <div name='Bdp' className='Bdp'>
                 <div className='content-Bdp'>
                     <div className='centered-content'>
-                        <h1 className='title-Bdp' data-aos="fade-down" data-aos-duration="1000">{t('Bdp.title')}</h1>
+                        <h1 className='title-Bdp' data-aos="fade-down" data-aos-duration="1000">
+                            {t('Bdp.title')}
+                        </h1>
                         <p className='text-Bdp' data-aos="zoom-in" data-aos-duration="1000">
                             {t('Bdp.description')}
                         </p>
-                        <button onClick={() => setShowModal(true)} className='centered-button' data-aos="fade-up" data-aos-duration="1000">{t('Bdp.button')}</button>
+                        <button onClick={() => setShowModal(true)} className='centered-button' data-aos="fade-up" data-aos-duration="1000">
+                            {t('Bdp.button')}
+                        </button>
                     </div>
                 </div>
             </div>
@@ -41,7 +48,7 @@ const Bdp = () => {
             <Fqa />
             <Footer hideFooterLinks={true} />
         </>
-    )
-}
+    );
+};
 
-export default Bdp
+export default Bdp;
