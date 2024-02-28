7ae67c7e329f8c629f193a94fd48c216c0587226
hamza boujid <hamzaboujid24@gmail.com>
2024-02-28 14:56:13 +0100

    update section b2academy team

--- src/components/ProfilesSection/ProfilesSection.jsx
+++ src/components/ProfilesSection/ProfilesSection.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import { FaLinkedin } from 'react-icons/fa';
 import './ProfilesSection.css';
 import imgPerson2 from '../../assets/imgPerson2.jpg';
+import { useTranslation } from 'react-i18next';
 
 function ProfilesSection() {
+      const { t } = useTranslation();
     return (
         <div className='ProfilesSection'>
             <div className='Title'>
-                <h3>TEAM</h3>
-                <h1>B2Academy Team</h1>
+                <h3>{t('ProfilesSection.title1')}</h3>
+                <h1>{t('ProfilesSection.title2')}</h1>
             </div>
             <div className='ProfileCardsContainer'>
                 <div className='ProfileCard'>
                     <img src={imgPerson2} alt='Mr. Moise' className='ProfileImage' />
                     <div className='ProfileInfo'>
                         <h3>Mr. Moise</h3>
-                        <p>Director of Operations</p>
+                        <p> {t('ProfilesSection.ProfileInfo1')}</p>
                         <a href='#' className='ProfileLink'>
                             <FaLinkedin />
                         </a>
@@ -25,7 +27,7 @@ function ProfilesSection() {
                     <img src={imgPerson2} alt='Mr. Ali' className='ProfileImage' />
                     <div className='ProfileInfo'>
                         <h3>Mr. Ali</h3>
-                        <p>President</p>
+                        <p> {t('ProfilesSection.ProfileInfo2')}</p>
                         <a href='#' className='ProfileLink'>
                             <FaLinkedin />
                         </a>
