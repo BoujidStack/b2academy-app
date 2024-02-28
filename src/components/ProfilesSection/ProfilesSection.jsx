a5dc375f3cd012dedb49936f30c614b564efa742
hamza boujid <hamzaboujid24@gmail.com>
2024-02-28 14:47:10 +0100

    update section b2academy team

--- src/components/ProfilesSection/ProfilesSection.jsx
+++ src/components/ProfilesSection/ProfilesSection.jsx
@@ -6,24 +6,30 @@ import imgPerson2 from '../../assets/imgPerson2.jpg';
 function ProfilesSection() {
     return (
         <div className='ProfilesSection'>
-            <div className='ProfileCard'>
-                <img src={imgPerson2} alt='Mr. Moise' className='ProfileImage' />
-                <div className='ProfileInfo'>
-                    <h3>Mr. Moise</h3>
-                    <p>Director of Operations</p>
-                    <a href='#' className='ProfileLink'>
-                        <FaLinkedin />
-                    </a>
-                </div>
+            <div className='Title'>
+                <h3>TEAM</h3>
+                <h1>B2Academy Team</h1>
             </div>
-            <div className='ProfileCard'>
-                <img src={imgPerson2} alt='Mr. Ali' className='ProfileImage' />
-                <div className='ProfileInfo'>
-                    <h3>Mr. Ali</h3>
-                    <p>President</p>
-                    <a href='#' className='ProfileLink'>
-                        <FaLinkedin />
-                    </a>
+            <div className='ProfileCardsContainer'>
+                <div className='ProfileCard'>
+                    <img src={imgPerson2} alt='Mr. Moise' className='ProfileImage' />
+                    <div className='ProfileInfo'>
+                        <h3>Mr. Moise</h3>
+                        <p>Director of Operations</p>
+                        <a href='#' className='ProfileLink'>
+                            <FaLinkedin />
+                        </a>
+                    </div>
+                </div>
+                <div className='ProfileCard'>
+                    <img src={imgPerson2} alt='Mr. Ali' className='ProfileImage' />
+                    <div className='ProfileInfo'>
+                        <h3>Mr. Ali</h3>
+                        <p>President</p>
+                        <a href='#' className='ProfileLink'>
+                            <FaLinkedin />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
