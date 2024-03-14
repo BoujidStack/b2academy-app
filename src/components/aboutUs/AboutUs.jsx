dd038a939a672dd6ba17e816182ddc8603600fa2
hamza boujid <hamzaboujid24@gmail.com>
2024-03-14 01:32:16 +0000

    add a page for ramadan promotion and fix prob of routes also add facebook pixel

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -9,33 +9,33 @@ const AboutUs = () => {
 
     return (
         <div name="aboutUs" className="aboutUs">
-            <div class="section">
-                <div class="titleAboutUs">
+            <div className="section">
+                <div className="titleAboutUs">
                     <h1 data-aos="fade-down" data-aos-duration="1000">{t('AboutUs.AboutUs')}</h1>
                 </div>
-                <div class="containerAboutUs">
-                    <div class="content-section" data-aos="fade-right" data-aos-duration="1000">
-                        <div class="title">
+                <div className="containerAboutUs">
+                    <div className="content-section" data-aos="fade-right" data-aos-duration="1000">
+                        <div className="title">
                             <h4>B<sup>2</sup>Academy</h4>
                         </div>
-                        <div class="content">
+                        <div className="content">
                             <p>B<sup>2</sup>Academy {t('AboutUs.description')}</p>
                         </div>
                     </div>
-                    <div class="pyramidImage-section" data-aos="fade-left" data-aos-duration="1000">
-                        <div class="pyramid-loader">
-                            <div class="wrapper">
-                                <span class="side side1"></span>
-                                <span class="side side2"></span>
-                                <span class="side side3"></span>
-                                <span class="side side4"></span>
-                                <span class="shadow"></span>
+                    <div className="pyramidImage-section" data-aos="fade-left" data-aos-duration="1000">
+                        <div className="pyramid-loader">
+                            <div className="wrapper">
+                                <span className="side side1"></span>
+                                <span className="side side2"></span>
+                                <span className="side side3"></span>
+                                <span className="side side4"></span>
+                                <span className="shadow"></span>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="video-container">
+            <div classNameName="video-container">
                 <YouTube videoId={videoId}/>
             </div>
         </div>
